2190ccc5dbb092648468ff673d15aa3555d44760
alfredsasko <alfred.sasko@gmail.com>
2020-02-03 15:33:02 +0100

    Create package set-up configuration in setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='advanced_pca',
+      version='0.1',
+      description='PCA with varimax rotation and feature selection',
+      packages=['advanced_pca'],
+      zip_safe=False)
