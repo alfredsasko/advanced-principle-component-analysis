a4c05f38b7b0f84c3aa2d4578d6dca5c28cfe41f
alfredsasko <alfred.sasko@gmail.com>
2020-02-04 10:50:53 +0100

    Add setup parameters to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,39 @@
 from setuptools import setup
+from os import path
 
-setup(name='advanced_pca',
-      version='0.1',
-      description='PCA with varimax rotation and feature selection',
-      packages=['advanced_pca'],
-      author='Alfred Sasko',
-      author_email='alfred.sasko@gmail.com',
-      zip_safe=False)
+# read the contents of your README file
+__file__ = 'C:/Users/Fredo/Google Drive/Knowledge Center/' \
+           'Data Scientist Nanodegree/pr-pypi-package/README.md'
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'advanced_pca',
+  packages = ['advanced_pca'],
+  version = '0.1',
+  license='MIT',
+  description = 'PCA with varimax rotation and feature selection '  \
+                'compatible with scikit-learn',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Alfred Sasko',
+  author_email = 'alfred.sasko@gmail.com',
+  url = 'https://github.com/alfredsasko',
+  keywords = ['Principle Component Analysis',
+              'Matrix rotation',
+              'Feature selection',
+              'PCA',
+              'scikit-learn'],
+  install_requires=['rpy2'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  zip_safe=False,
+)
